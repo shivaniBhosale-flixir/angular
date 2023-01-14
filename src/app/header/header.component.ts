573acf573264c422d9aef898842554a52ae3de3b
shivaniBhosale-flixir <shivani.s.bhosale95@gmail.com>
2023-01-14 19:33:26 +0530

    chnages

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  logout(){
+    sessionStorage.removeItem('token');
+    sessionStorage.clear();
+    localStorage.clear();
+  }
 }
